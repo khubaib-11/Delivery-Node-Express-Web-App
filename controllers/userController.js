1d6c6fdedd742146cbb4a1cb566a51a214e1b50c
khubaib-11 <81750667+khubaib-11@users.noreply.github.com>
2022-08-16 23:48:52 -0700

    Add : create user handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,3 +25,14 @@ exports.getOneUser = catchAsync(async (req, res, next) => {
   });
 });
 
+// Create a User
+exports.createUser = catchAsync(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
