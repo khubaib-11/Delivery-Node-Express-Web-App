a9ca36936d2b5ed6c4b956535e62521aa85fd42a
khubaib-11 <81750667+khubaib-11@users.noreply.github.com>
2022-08-19 02:35:10 -0700

    Add : Unhandled routes handler + Global Error Handler

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+//* Middleware -
 app.use(express.json());
 
+//* Routes -
 app.use('/api/v1/users', userRouter);
+
+//? Unhandled route -
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
+});
+
+//? Global Error Handling Middleware -
+app.use(globalErrorHandler);
 
 module.exports = app;
