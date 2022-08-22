7f8ff4571255a58d4b70d42133bff14a290d0d2a
khubaib-11 <81750667+khubaib-11@users.noreply.github.com>
2022-08-22 03:42:53 -0700

    Add : added signUp route & its handler

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+// SignUp and Login Routes
+router.post('/signup', authController.signUp);
 
 router
   .route('/')
