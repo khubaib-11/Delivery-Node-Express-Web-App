d68b6628809e7aedd20ea5d37d4b1d188639a837
khubaib-11 <81750667+khubaib-11@users.noreply.github.com>
2022-08-22 03:44:50 -0700

    Add : created login handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,3 +24,33 @@ exports.signUp = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+// Log In
+exports.login = catchAsync(async (req, res, next) => {
+  //Step 1) Check if user has provided email and password or not
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide an email and a password.', 400));
+  }
+
+  //Step 2) Check any user with this email exists in DB
+  const foundUser = await User.findOne({ email }).select('+password');
+
+  if (
+    !foundUser ||
+    !(await foundUser.matchPassword(password, foundUser.password))
+  ) {
+    return next(new AppError('Incorrect email or password!', 401)); // 401 means unauthorized
+  }
+
+  //Step 2) If everything is OK, generate a token
+  const token = jwt.sign({ id: foundUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
