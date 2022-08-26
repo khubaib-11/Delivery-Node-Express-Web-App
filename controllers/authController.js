dfc2a88f70a42c0994278becd77dc4f7c894f4bf
khubaib-11 <81750667+khubaib-11@users.noreply.github.com>
2022-08-26 03:29:10 -0700

    Add + Change : added route protection + changed signUp  method's input data

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
@@ -7,9 +8,11 @@ const AppError = require('../utils/appError');
 exports.signUp = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
+    role: req.body.role,
     email: req.body.email,
     password: req.body.password,
-    // passwordConfirm: req.body.passwordConfirm,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
   });
 
   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
@@ -54,3 +57,45 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+// Protected Routes handler
+
+// --------------
+// Main Functionality ----> protect certain routes from non-logged in users.
+// --------------
+
+exports.protectedRoute = catchAsync(async (req, res, next) => {
+  // Step 1) Check if user has provided the token & it starts with "Bearer" word.
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith('Bearer')
+  ) {
+    return next(new AppError('Token no found. Please login first.'));
+  }
+  const token = req.headers.authorization.split(' ')[1];
+
+  // Step 2) Verification of token via JWT
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // Step 3) Check any user with this token's id exist
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401
+      )
+    );
+  }
+
+  // Step 4) Check if user has changed his password after get getting his token or not
+  if (currentUser.passwordChangedAfterTokenReceived(currentUser.iat)) {
+    return next(
+      new AppError('User recently changes password! Please login again', 401)
+    );
+  }
+  next();
+});
+
+
