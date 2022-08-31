60439071519e652c4d02ef15c13624ec9e313d50
khubaib-11 <81750667+khubaib-11@users.noreply.github.com>
2022-08-31 09:44:24 -0700

    Add : new Schema fields + new document & query middleware + instance methods

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
@@ -30,45 +31,67 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordModifiedAt: Date,
+  passwordResetToken: String,
+  passwordResetTokenExpires: Date,
+  accountActive: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 //! Document Middleware -
 
-// Encrypting Passwords before saving in DB
+// ===========
+// 1) Encrypting Passwords before saving in DB
+// ===========
+
 userSchema.pre('save', async function (next) {
   //Step 1) If password is not modified don't hash it, go into next middleware
   if (!this.isModified('password')) {
     return next();
   }
 
-  //Step 2)  If password is modified, hash it
-  this.password = await bcrypt.hash(this.password, 12);
-  this.passwordModifiedAt = Date.now();
+  //Step 2)  If password is modified :
+  // Hash the password with cost of 12
 
-  // Step 3) Add passwordChangedAt property
-  // this.passwordChangedAt = Date.now();
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Step 3) Delete passwordConfirm field
+  this.passwordConfirm = undefined;
   next();
 });
 
-// userSchema.pre(
-//   'updateOne',
-//   { document: true, query: false },
-//   async function (next) {
-//     if (!this.isModified('password')) {
-//       console.log('Not modified');
-//       return next();
-//     }
-//     console.log('Updating ............');
+// ===========
+// 2) Setting time, when password is changed
+// ===========
 
-//     // Step 3) Add passwordChangedAt property
-//     this.passwordModifiedAt = Date.now();
-//     next();
-//   }
-// );
+userSchema.pre('save', function (next) {
+  // Password is not modified or new, don't do anything
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordModifiedAt = Date.now() - 3000;
+
+  next();
+});
+
+//! Query Middleware -
+
+// ===========
+// 1) Only show active accounts in results
+// ===========
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ accountActive: { $ne: false } });
+  next();
+});
 
 //! Instance Methods -
 
-// 1) Match passwords
+// ===========
+// 1) Matching passwords
+// ===========
+
 userSchema.methods.matchPassword = async function (
   enteredPassword,
   DBpassword
@@ -76,21 +99,40 @@ userSchema.methods.matchPassword = async function (
   return await bcrypt.compare(enteredPassword, DBpassword);
 };
 
-// 2)
+// ===========
+// 2) Checking if password is changed after token is issued
+// ===========
+
 userSchema.methods.passwordChangedAfterTokenReceived = function (
   tokenIssuedAt
 ) {
   if (this.passwordChangedAt) {
-    const dateConvertedToTime = parseInt(
+    const passwordChangedDateIntoTime = parseInt(
       this.passwordChangedAt.getTime() / 1000,
       10
     );
 
-    return tokenIssuedAt < this.passwordChangedAt; // 10am < 12am
+    return tokenIssuedAt < passwordChangedDateIntoTime; // 10am < 12am
   }
 
   // false means password not changes
   return false;
+};
+
+// ===========
+// 3) Generating token for password reset handler
+// ===========
+
+userSchema.methods.generatePasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetTokenExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
